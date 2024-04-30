708b9b245df5b2f6d6ff33ccbd0282194fcc7d32
AMANSHARMArdk <amansharma.as781485@gmail.com>
2024-04-30 11:45:55 +0530

    new file update is html

---
+++ python.py
@@ -0,0 +1,25 @@
+# write the program for martix addition     
+# write the program for martix multiplication
+
+# write the program for martix transpose
+
+
+# write the program for martix determinant
+
+
+
+# write the program for martix inverse
+
+
+
+
+# write the program for martix rank
+
+
+
+
+
+# write the program for martix eigen value      
+# write the program for martix eigen vector
+
+        
